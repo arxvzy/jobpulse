387e39eb88ff03bacd6d548378a1344a4cd4e05c
Arxvzy <szaryabima@gmail.com>
2025-06-13 05:00:10 +0700

    UI: add icon

---
+++ resources/views/svg/mail.blade.php
@@ -0,0 +1,4 @@
+<svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+        d="M21.75 5.25v13.5a2.25 2.25 0 01-2.25 2.25H4.5a2.25 2.25 0 01-2.25-2.25V5.25A2.25 2.25 0 014.5 3h15a2.25 2.25 0 012.25 2.25zM3.75 5.25l8.25 6.75 8.25-6.75" />
+</svg>
---
+++ resources/views/svg/status.blade.php
@@ -0,0 +1,4 @@
+<svg class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+        d="M12 6V3m0 0a9 9 0 110 18m0-18a9 9 0 000 18m0 0v-3" />
+</svg>
